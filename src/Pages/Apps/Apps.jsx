fc09d4ad51b5378d131fb8f93ecc5bb194bfaa69
Shashuvo <shahariat2810@gmail.com>
2026-05-27 23:25:25 +0600

    all page name and description added

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Apps = () => {
     return (
-        <div>
-            <h1>This is all apps page</h1>
+        <div className='mt-20 w-5/6 mx-auto'>
+            {/* title & description */}
+            <div className='space-y-4 text-center mb-10'>
+                <h1 className='font-bold text-5xl'>Our All Applications</h1>
+                <p className='text-[#627382] text-xl'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            </div>
         </div>
     );
 };
