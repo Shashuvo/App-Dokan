08f3a1770ec91c79b142da08b5f400a54857838d
Shashuvo <shahariat2810@gmail.com>
2026-06-02 21:14:48 +0600

    installation page info added

--- src/Pages/Installation/Installation.jsx
+++ src/Pages/Installation/Installation.jsx
@@ -6,8 +6,22 @@ const Installation = () => {
     const app = useLoaderData();
     console.log(installedIds, app);
     return (
-        <div>
-            <h1>this is installation page</h1>
+        <div div className='my-20 w-5/6 mx-auto'>
+            {/* title & description */}
+            <div className='space-y-4 text-center mb-10'>
+                <h1 className='font-bold text-5xl'>Your Installed Apps</h1>
+                <p className='text-[#627382] text-xl'>Explore All Trending Apps on the Market developed by us</p>
+            </div>
+            {/* app counts & sort apps */}
+            <div className='mb-10 flex justify-between'>
+                <span className='text-2xl font-semibold'>({1}) Apps Found</span>
+                <span>
+                    <select defaultValue="Sort By Size" className="select select-[#627382] text-[#627382] bg-transparent">
+                        <option>Sort By Size</option>
+                        <option>Sort By Downloads</option>
+                    </select>
+                </span>
+            </div>
         </div>
     );
 };
