5831ec07606db6de6c3c4b805bce05b450e26295
Shashuvo <shahariat2810@gmail.com>
2026-06-06 21:04:45 +0600

    local storage install handled

--- src/Pages/AppsDetails/AppsDetails.jsx
+++ src/Pages/AppsDetails/AppsDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BiDownload } from 'react-icons/bi';
 import { FaStar } from 'react-icons/fa';
 import { MdReviews, MdSdStorage } from 'react-icons/md';
@@ -7,34 +7,27 @@ import RatingChart from '../../Components/RatingChart/RatingChart';
 import { ToastContainer, toast } from 'react-toastify';
 import { addToInstalledApps } from '../../utilities/AddtoDB/AddToDB';
 
-
 const AppsDetails = () => {
     const { id } = useParams();
-
-    const [installed, setInstalled] = useState(false);
-
-    const { setInstalledIds } = useOutletContext();
-
+    const { installedIds, setInstalledIds } = useOutletContext();
     const apps = useLoaderData();
 
     const app = apps.find(app => app.id === parseInt(id));
-
     const { image, title, companyName, description, size, reviews, ratingAvg, downloads, ratings } = app;
 
+    const installed = installedIds.map(String).includes(String(app.id));
+
     const handleInstall = () => {
-        const success = addToInstalledApps(app.id, setInstalledIds); // ← save to localStorage + sync state
+        const success = addToInstalledApps(app.id, setInstalledIds);
         if (success) {
-            setInstalled(true);
             toast.success(`${title} installed successfully!`);
         }
-    }
-
-
+    };
 
     return (
         <div className="min-h-screen my-20">
-            <ToastContainer />
-            <div className=" w-5/6 mx-auto flex flex-col gap-10 bg-transparent">
+            <ToastContainer></ToastContainer>
+            <div className="w-5/6 mx-auto flex flex-col gap-10 bg-transparent">
 
                 {/* Top Section */}
                 <div className="flex flex-col md:flex-row gap-10">
@@ -49,7 +42,6 @@ const AppsDetails = () => {
                         {/* name & developer */}
                         <div className='flex-1 space-y-2'>
                             <h1 className="text-4xl font-bold text-[#001931]">{title}</h1>
-
                             <p className="text-xl text-[#627382]">
                                 Developed by <span className='bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent font-semibold'>{companyName}</span>
                             </p>
@@ -84,7 +76,11 @@ const AppsDetails = () => {
                             </div>
                         </div>
 
-                        <button onClick={handleInstall} disabled={installed} className={`mt-4 ${installed ? "bg-[#627382] cursor-not-allowed" : "bg-[#00D390] hover:bg-green-600 cursor-pointer"} text-xl font-semibold text-white px-5 py-2 rounded-lg`}>
+                        <button
+                            onClick={handleInstall}
+                            disabled={installed}
+                            className={`mt-4 ${installed ? "bg-[#627382] cursor-not-allowed" : "bg-[#00D390] hover:bg-green-600 cursor-pointer"} text-xl font-semibold text-white px-5 py-2 rounded-lg`}
+                        >
                             {installed ? "Installed" : `Install Now (${size} MB)`}
                         </button>
                     </div>
@@ -93,14 +89,13 @@ const AppsDetails = () => {
                 <div className="divider"></div>
 
                 {/* App Review Chart */}
-                <RatingChart ratings={ratings}></RatingChart>
+                <RatingChart ratings={ratings} />
 
                 <div className="divider"></div>
 
                 {/* Description */}
                 <div className="space-y-6">
                     <h2 className="text-2xl font-semibold text-[#001931]">Description</h2>
-
                     <p className="text-[#627382] text-xl whitespace-pre-line">
                         {description}
                     </p>
